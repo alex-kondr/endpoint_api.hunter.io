c300ab7b3a60b16a0aa22baf65a0d3ea7c4eb2e3
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-01-08 11:00:26 +0200

    save

--- src/routes/find_verify_email.py
+++ src/routes/find_verify_email.py
@@ -1,6 +1,4 @@
-import json
-
-from requests import request
+import requests
 from fastapi import APIRouter, Depends
 
 from src.conf.config import settings
@@ -13,9 +11,15 @@ router = APIRouter(prefix="/email", tags=["email"])
 @router.get("/find", name="Return email if find person", response_model=Email)
 async def find_email(body: EmailModel = Depends()):
 
-    url = f"https://api.hunter.io/v2/email-finder?domain={body.domain}&first_name={body.first_name}&last_name={body.last_name}&api_key={settings.api_hunter_io}"
-    response = request(method="GET", url=url)
-    email = json.loads(response.content).get("data", {}).get("email", "Not found")
+    params = {
+        "domain": body.domain,
+        "first_name": body.first_name,
+        "last_name": body.last_name,
+        "api_key": settings.api_hunter_io
+    }
+    url = f"https://api.hunter.io/v2/email-finder"
+    response = requests.get(url=url, params=params)
+    email = response.json().get("data", {}).get("email", "Not found")
 
     return {"email": email}
 
@@ -23,8 +27,12 @@ async def find_email(body: EmailModel = Depends()):
 @router.get("/verify", name="Returns the email verification status", response_model=EmailVerify)
 async def verify_email(body: Email = Depends()):
 
-    url = f"https://api.hunter.io/v2/email-verifier?email={body.email}&api_key={settings.api_hunter_io}"
-    response = request(method="GET", url=url)
-    status = json.loads(response.content).get("data", {}).get("status", "Not found")
+    params = {
+        "email": body.email,
+        "api_key": settings.api_hunter_io
+    }
+    url = f"https://api.hunter.io/v2/email-verifier"
+    response = requests.get(url=url, params=params)
+    status = response.json().get("data", {}).get("status", "Not found")
 
     return {"status": status}
